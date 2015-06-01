a05acbff13569063e7d0991967ad8e134984cbdd
Florian <florian@glatz-mahlberg.de>
2015-06-01 03:30:51 +0200

    started mailchimp integration, currently via ajax submissio of standard form. fails bc of xhr problem.

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -1,123 +1,139 @@
-var
-  wedding = '',
+$(document).ready(function() {
+  var
+    wedding = '',
 
-  animation = (function () { // animation control
-    var transitionSpeed = 1000, // in ms
-        bgcanvas = $('body'),
-        img_path = 'img/';
+    animation = (function () { // animation control
+      var transitionSpeed = 1000, // in ms
+          bgcanvas = $('body'),
+          img_path = 'img/';
 
-    // settings
-    $.fx.speeds._default = transitionSpeed;
-    $.fn.setCursorPosition = function(pos) {
-      this.each(function(index, elem) {
-        if (elem.setSelectionRange) {
-          elem.setSelectionRange(pos, pos);
-        } else if (elem.createTextRange) {
-          var range = elem.createTextRange();
-          range.collapse(true);
-          range.moveEnd('character', pos);
-          range.moveStart('character', pos);
-          range.select();
+      // settings
+      $.fx.speeds._default = transitionSpeed;
+      $.fn.setCursorPosition = function(pos) {
+        this.each(function(index, elem) {
+          if (elem.setSelectionRange) {
+            elem.setSelectionRange(pos, pos);
+          } else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+          }
+        });
+        return this;
+      };          
+
+      // handlers
+      function reset (o) {
+        o = $(o).stop();
+
+        if ( o.css('opacity') > 0) {
+          o.transition({opacity: 0}, function () {
+            o.value = o.value || wedding;
+          });
         }
-      });
-      return this;
-    };          
+        else o.value = o.value || wedding;
+      }
 
-    // handlers
-    function reset (o) {
-      o = $(o).stop();
+      function start (o) {
+        $(o)
+          .transition({opacity: 1})
+          .typed({
+            strings: [
+              // 'faces',
+              // 'events',
+              // 'locations',
+              // 'subscribe now',
+              ''
+            ],
+            contentType: 'html',
+            typeSpeed: 50,
+            startDelay: transitionSpeed/2,
+            loop: false,
+            showCursor: true,
+            cursorChar: '|',
+            callback: function () {
+              input(o);
+            },
+            preStringTyped: function () {
+              $(o).addClass('animation')
+            },
+            resetCallback: function () {
+              $(o).removeClass('animation')
+            }
+          });
+      }
 
-      if ( o.css('opacity') > 0) {
-        o.transition({opacity: 0}, function () {
-          o.value = o.value || wedding;
+      function input (o) {
+        var submission = false;
+
+        $(o)
+          .val('@')
+          .setCursorPosition(0)
+          .on('keypress', function (event) {
+            if (event.keyCode == 13 && !submission) {
+              submission = true;
+              $.ajax({
+                method: 'POST',
+                url: 'http://wedding.us11.list-manage.com/subscribe/post?u=e47873f5433b478ab722f8608&amp;id=a4d95778c2',
+                data: { EMAIL: o.value }
+              })
+                .done(function () {
+                  thankyou(o);
+                })
+                .fail(function () {
+                  o.value = 'error, try again!'
+                  submission = false;
+                });
+            }
+          })
+          .focus();
+      }
+
+      function bg (img) {
+        bgcanvas.css({
+          'backgroundImage': 'url(' + img_path + img + ')',
+          'backgroundSize': '100%',
+          'backgroundPosition': '0 0'
         });
       }
-      else o.value = o.value || wedding;
-    }
 
-    function start (o) {
-      $(o)
-        .transition({opacity: 1})
-        .typed({
-          strings: [
-            'people',
-            'events',
-            'locations',
-            'subscribe now',
-            ''
-          ],
-          contentType: 'html',
-          typeSpeed: 50,
-          startDelay: transitionSpeed/2,
-          loop: false,
-          showCursor: true,
-          cursorChar: '|',
-          callback: function () {
-            input(o);
-          },
-          preStringTyped: function () {
-            $(o).addClass('animation')
-          },
-          resetCallback: function () {
-            $(o).removeClass('animation')
-          }
-        });
-    }
+      function thankyou(o) {
+        bg('kiss.png');
+        var msg = $('<div class="thanks">thank you</div>')
+          .typed({
+            strings: [
+              'we sent you a confirmation email', 
+              'thank you',
+              ':)'
+            ],
+            contentType: 'html',
+            typeSpeed: 50,
+            startDelay: 0,
+            loop: false,
+            showCursor: true,
+            cursorChar: '|'
+          });
 
-    function input (o) {
-      $(o)
-        .val('@')
-        .setCursorPosition(0)
-        .keypress(function (event) {
-          if (event.keyCode == 13) { // use type conversion in case keyCode delivered as string in other browsers
-            thankyou(o);
-          }
-        })        
-        .focus();
-    }
+        $(o).replaceWith(msg);
+      }
 
-    function bg (img) {
-      bgcanvas.css({
-        'backgroundImage': 'url(' + img_path + img + ')',
-        'backgroundSize': '100%',
-        'backgroundPosition': '0 0'
+      // api
+      return {
+        start: start,
+        reset: reset,
+        input: input
+      }
+    })(),
+
+    // the canvas for the animation (=input field)
+    canvas = $('<input type="text" name="EMAIL" dir="ltr" value="' + wedding + '">')
+      .prependTo('body')
+      .on('click', function () {
+        animation.start(this);
       });
-    }
 
-    function thankyou(o) {
-      bg('kiss.png');
-      var msg = $('<div class="thanks">thank you</div>')
-        .typed({
-          strings: [
-            'we sent you a confirmation email',
-            'thank you',
-            ':)'
-          ],
-          contentType: 'html',
-          typeSpeed: 50,
-          startDelay: 0,
-          loop: false,
-          showCursor: true,
-          cursorChar: '|'
-        });
-
-      $(o).replaceWith(msg);
-    }
-
-    // api
-    return {
-      start: start,
-      reset: reset,
-      input: input
-    }
-  })(),
-
-  // the canvas for the animation (=input field)
-  canvas = $('<input type="text" name="email" dir="ltr" value="' + wedding + '">')
-    .prependTo('body')
-    .on('click', function () {
-      animation.start(this);
-    });
-
-  // initialize canvas
-  animation.reset(canvas);
+    // initialize canvas
+    animation.reset(canvas);
+});
